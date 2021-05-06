1ed3dcbad2e8607b00870210fb5a4a8354711532
jihoon <jihoon2723@naver.com>
2021-05-06 23:50:29 +0900

    test

---
+++ src/test/java/jihoon/planner_web/PlannerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package jihoon.planner_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
