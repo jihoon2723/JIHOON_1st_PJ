c94eb07b8d54ef96bdfd8e9af5fb932fe998954b
jihoon <jihoon2723@naver.com>
2021-05-06 23:50:29 +0900

    test

---
+++ src/main/java/jihoon/planner_web/PlannerWebApplication.java
@@ -0,0 +1,13 @@
+package jihoon.planner_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerWebApplication.class, args);
+	}
+
+}
