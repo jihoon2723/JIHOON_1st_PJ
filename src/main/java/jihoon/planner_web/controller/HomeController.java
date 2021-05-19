9da37760ebb224cada05924037bc56cff6eae2ba
jihoon <jihoon2723@naver.com>
2021-05-19 22:12:20 +0900

    회원가입 로그인 기능 완성 세션 관리 미흡/ 메인화면 로그인 시 로그인/회원가입 버튼 사라지고 로그아웃 버튼 생기게하기

--- src/main/java/jihoon/planner_web/controller/HomeController.java
+++ src/main/java/jihoon/planner_web/controller/HomeController.java
@@ -2,6 +2,7 @@ package jihoon.planner_web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -11,9 +12,5 @@ public class HomeController {
 
         return "index";
     }
-//    @GetMapping("/loginForm")
-//    public String loginForm() {
-//
-//        return "login_joinForm";
-//    }
+
 }
