2ddb724fe72ca630b4fda5851381d8223290e60d
jihoon <jihoon2723@naver.com>
2021-06-11 17:54:05 +0900

    프로젝트 주제 변경에 따른 로고 변경

--- src/main/java/jihoon/planner_web/controller/MemberController.java
+++ src/main/java/jihoon/planner_web/controller/MemberController.java
@@ -1,6 +1,5 @@
 package jihoon.planner_web.controller;
 
-import jihoon.planner_web.domain.MemberInfo;
 import jihoon.planner_web.dto.MemberInfoDto;
 import jihoon.planner_web.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -22,8 +20,6 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class MemberController {
     private final MemberService memberService;
-
-
     @PostMapping("/user")
     public String signup(MemberInfoDto infoDto) {//회원 추가
         memberService.save(infoDto);
